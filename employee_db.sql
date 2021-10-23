b83459931d7294fe4fe4ca4b6a8c2fbfb000be12
Brent Graves <graves.brent@gmail.com>
2021-10-23 15:32:45 -0400

    added two tables to sql roles and employee.

--- employee_db.sql
+++ employee_db.sql
@@ -11,3 +11,20 @@ CREATE TABLE department (
   name VARCHAR(50) NOT NULL,
   -- PRIMARY KEY (id)
 );
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT, 
+  -- PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR (50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT NOT NULL, 
+  manager_id INT, 
+  PRIMARY KEY (id)
+);
