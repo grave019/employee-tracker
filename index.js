ef985474531c5869972dbab737a5cf49a8bc5b4a
Brent Graves <graves.brent@gmail.com>
2021-10-24 19:47:47 -0400

    added user name password and port to set up functions for interconnectivity.

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ const figlet = require("figlet");
 // Secure connection
 const Sequelize = require("sequelize");
 require("dotenv").config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3001,
+    }
+  );
+
+  // Variables
+let roles;
+let departments;
+let managers;
+let employees;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3001
+  port: 3001,
+
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: "employees_db",
+});
