65afb1d2cc3d399f05b9c2d0ed9eb2c58235f49a
Brent Graves <graves.brent@gmail.com>
2021-10-24 19:52:44 -0400

    added inital prompts for inquirer.

--- index.js
+++ index.js
@@ -52,3 +52,33 @@ connection.connect(function (err) {
     getManagers();
     getEmployees();
   });
+
+  // Intiating prompts
+start = () => {
+    inquirer
+      .prompt({
+        name: "choices",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"],
+      })
+      .then(function (answer) {
+        if (answer.choices === "ADD") {
+          addSomething();
+        } else if (answer.choices === "VIEW") {
+          viewSomething();
+        } else if (answer.choices === "UPDATE") {
+          updateSomething();
+        } else if (answer.choices === "DELETE") {
+          deleteSomething();
+        } else if (answer.choices === "EXIT") {
+          figlet("Exiting Employee Tracker", (err, result) => {
+            console.log(err || result);
+          });
+  
+          connection.end();
+        } else {
+          connection.end();
+        }
+      });
+  };
